796dfa6e18d8aba25d27501bb7d4281ca6642f8b
Vladimir <vladimir.shpinev.1981@gmail.com>
2024-01-25 14:43:18 +0200

    Добавьте файлы проекта.

---
+++ Models/TelegramWebAppInitData.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Models
+{
+    public class TelegramWebAppInitData
+    {
+        public string query_id { get; set; }
+        public TelegramWebAppUser user { get; set; }
+        public TelegramWebAppUser receiver { get; set; }
+        public TelegramWebAppChat chat { get; set; }
+        public string chat_type { get; set; }
+        public string chat_instance { get; set; }
+        public int auth_date { get; set; }
+        public string hash { get; set; }
+    }
+}
---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using WebApp.Models;
+
+namespace WebAppShop.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        public TelegramWebAppInitData _webAppInitData;
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet(TelegramWebAppInitData webAppInitData)
+        {
+            _webAppInitData = webAppInitData;
+        }
+    }
+
+}
